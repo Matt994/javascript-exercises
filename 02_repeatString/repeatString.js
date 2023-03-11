5feb6e326bc5243406398369b9df4d0550796e17
Mateusz Soszynski <soszynski94@googlemail.com>
2023-03-11 18:25:34 +0000

    exercises 01 and 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let str = "";
+    while (i < num) {
+        str += string;
+        i++;
+    }
+    
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return str;
+    }
+    
 };
 
 // Do not edit below this line
