b959def19b8b8092dbb2f6df016342a3b3698c8c
Mateusz Soszynski <soszynski94@googlemail.com>
2023-03-20 18:11:24 +0000

    tempConversion exercise convert to celcius done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  cel = (far - 32) * 5/9
+  if (!Number.isInteger(cel)) {
+    round = Math.round(cel * 10) / 10;
+    return round;
+  } else {
+    return cel;
+  }
+  
 };
 
 const convertToFahrenheit = function() {
